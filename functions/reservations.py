63b6b01a7b28fc020b8218389b608cf10ebae2cf
Dave <davethebrave@gmail.com>
2025-03-22 16:45:03 +0100

    [WIP] started check_reservation_expiration method

--- functions/reservations.py
+++ functions/reservations.py
@@ -1,5 +1,6 @@
 from firebase_functions import https_fn
 from google.cloud import firestore
+from google.cloud.firestore_v1.document import DocumentReference
 
 def send_reservation_confirmation(res_id: str) -> https_fn.Response:
     print(res_id)
@@ -24,3 +25,24 @@ def send_reservation_confirmation(res_id: str) -> https_fn.Response:
     print(user_data)
 
     return https_fn.Response("OK", 200)
+
+def check_reservation_expiration(reservation_id: str) -> https_fn.Response:
+    db = firestore.Client()
+    doc_ref = db.collection("event_reservation").document(reservation_id)
+    res_doc = doc_ref.get()
+    if not res_doc.exists:
+        print ("Reservation " + reservation_id + " not found")
+        return https_fn.Response("Reservation not found", 404)
+    
+    res_data = res_doc.to_dict()
+    
+    if res_data.get("confirmed") is True:
+        print ("Reservation " + reservation_id + " already confirmed")
+        return https_fn.Response("Reservation already confirmed", 200)
+    
+    if "createdAt" not in res_data:
+        print ("Reservation " + reservation_id + " created at not found. Deleting...")
+        doc_ref.delete()
+        return https_fn.Response("Reservation deleted", 200)
+    
+    return https_fn.Response("Reservation not confirmed", 400)
