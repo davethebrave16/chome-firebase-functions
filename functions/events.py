8795f304cc1c5af3840c611713ab89280437b70a
Dave <davethebrave@gmail.com>
2025-03-22 10:48:51 +0100

    completed event duplicate function

--- functions/events.py
+++ functions/events.py
@@ -8,19 +8,25 @@ def duplicate_event_associations(new_event_id: str, old_event_id: str) -> https_
     print('Trying to duplicate event associations from ' + old_event_id + ' to ' + new_event_id)
     
     db = firestore.Client()
-    
-    old_questions = db.collection("event_survey_question").where("event", "==", '/event/' + old_event_id).stream()
-
-    print('Questions found: ' + str(len(list(old_questions))))
+    old_event_ref = db.collection("event").document(old_event_id)
+    if not old_event_ref.get().exists:
+        return https_fn.Response("Old event not found", 404)
+    new_event_ref = db.collection("event").document(new_event_id)
+    if not new_event_ref.get().exists:
+        return https_fn.Response("New event not found", 404)
+    print('Old event found: ' + old_event_ref.id + ' with title ' + old_event_ref.get().to_dict()["name"])
+    print('New event found: ' + new_event_ref.id + ' with title ' + new_event_ref.get().to_dict()["name"])
+    old_questions = list(db.collection("event_survey_question").where("event", "==", old_event_ref).stream())
+    print('Questions found:', len(old_questions))
 
     for question in old_questions:
         data = question.to_dict()
-        print('Duplicating question: ' + data.id + ' with text ' + data["questionText"])
-        data["event"] = '/event/' + new_event_id
+        print('Duplicating question: ' + question.id + ' with text ' + data["questionText"])
+        data["event"] = new_event_ref
         question_ref = db.collection("event_survey_question").document()
         question_ref.set(data)
         if question_ref.get().exists:
-            print('Question duplicated')
+            print('Question duplicated with id ' + question_ref.id)
         else:
             print('Question not duplicated')
         
