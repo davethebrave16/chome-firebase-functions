45b9cac95f422d1ea39c41766088c8dc55798a27
Dave <davethebrave@gmail.com>
2025-09-12 14:31:14 +0200

    fixed library

--- functions/src/events/__init__.py
+++ functions/src/events/__init__.py
@@ -1,5 +1,5 @@
 """Events module for Chome Firebase Functions."""
 
-from .event_service import EventService, duplicate_event_associations, delete_event_associations
+from .event_service import EventService, duplicate_event_associations, delete_event_associations, process_event_position
 
-__all__ = ["EventService", "duplicate_event_associations", "delete_event_associations"]
+__all__ = ["EventService", "duplicate_event_associations", "delete_event_associations", "process_event_position"]
